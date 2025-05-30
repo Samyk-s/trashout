24ab6cb49c68268e3830f78ff58323466d705482
Samyk-s <samykbajr919@gmail.com>
2025-05-30 05:55:46 +0545

    s

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import Routing from "./routing/routing.config.jsx";
-import "flowbite/dist/flowbite.min.css";
-import { Analytics } from "@vercel/analytics/next";
-createRoot(document.getElementById("root")).render(
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import Routing from './routing/routing.config.jsx';
+import 'flowbite/dist/flowbite.min.css';
+import { Analytics } from '@vercel/analytics/react';  // Import Vercel Analytics
+
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* Add the Analytics component here so it tracks visits across the entire app */}
     <Analytics />
     <Routing />
-  </StrictMode>
+  </StrictMode>,
 );
